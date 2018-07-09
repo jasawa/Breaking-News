cea1748252573781700fe13118f2ffcf1b9c66f9
Jane Asawa <asawajane@gmail.com>
2018-07-08 21:57:31 -0700

    changes

---
+++ public/assets/js/index.js
@@ -0,0 +1,23 @@
+
+$(document).ready(function() {
+    function showHeadlines() {
+        // Grab the articles as a json
+        $.getJSON("/articles", function(data) {
+            // For each one
+            for (var i = 0; i < data.length; i++) {
+            // Display the apropos information on the page
+            $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+            }
+        });
+    }
+
+    showHeadlines();
+
+    function scrapeHeadlines() {
+        
+    }
+
+    $("#scrapeBtn").on("click", function() {
+       scrapeHeadlines();
+    })
+}) 
